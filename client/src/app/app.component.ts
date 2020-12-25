806f1a09c987fe02898960b182c7a187132f089b
Ebrailson Gabriel <ebrailsongil@hotmail.com>
2020-12-25 20:02:37 -0100

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/products';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ constructor(private http:HttpClient){}
   title = 'client';
+products:IProduct[];
+
+  ngOnInit(): void {
+
+   this.http.get('https://localhost:5001/api/products').subscribe((response:IPagination)=>{
+this.products=response.data;
+   }, error=>{
+    console.log(error)
+   })
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
